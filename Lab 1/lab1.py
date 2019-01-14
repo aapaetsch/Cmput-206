420e6206148429d9cf3708f6770d057bcf9efc9a
Aidan Paetsch <aapaetsch@gmail.com>
2019-01-14 16:47:27 -0700

    stuff

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -1,11 +1,26 @@
 import numpy as np
 from scipy import ndimage, misc
+import skimage.io as sk
 
 
 def main():
 	imgArray = np.zeros(256, dtype = int)
 	print(imgArray)
-	img = misc
+	a = sk.imread('test.jpg', True)
+	u = 0
+	for i in a:
+		u+=1
+		j =0
+		for x in i:
+			j+=x
+		j = int(round(j))
+
+		imgArray[j]+=1
+
+	print(imgArray)
+
+
+	
 
 
 
